000cc660836c95bdf0c43943d23d085ecb3b28fc
linushu <lyricjin@foxmail.com>
2022-10-11 19:30:46 +0800

    盲冲一波, 我要先把网络测试出来, 然后还要处理抖动啥的

--- Assets/_Scripts/FrameDataDef/HeartbeatCmd.cs
+++ Assets/_Scripts/FrameDataDef/HeartbeatCmd.cs
@@ -6,7 +6,12 @@ using UnityEngine;
 [MessagePackObject]
 public class HeartbeatCmd
 {
+    [Key(0)]
     public int Index;
+
+    [Key(1)]
     public float SendTime;
+
+    [Key(2)]
     public float RecvTime;
 }
